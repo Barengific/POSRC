8e3104b73aba2c226b337b0a9fc186f663b41894
Barengific <75044077+Barengific@users.noreply.github.com>
2022-05-04 20:30:30 +0100

    Update MainPage.xaml.cs

--- ap/MainPage.xaml.cs
+++ ap/MainPage.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
@@ -12,6 +13,7 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using Microsoft.Data.Sqlite;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
@@ -25,6 +27,55 @@ namespace ap
         public MainPage()
         {
             this.InitializeComponent();
+        }
+
+        private void BtnLogin(object sender, RoutedEventArgs e)
+        {
+
+            Debug.WriteLine("___: ");
+            using (var connection = new SqliteConnection("Data Source=hello.db"))
+            {
+                connection.Open();
+
+                var command = connection.CreateCommand();
+
+                command.CommandText = @"SELECT id, upass, job_title FROM staff";
+
+                using (var reader = command.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        var id = reader.GetString(0);
+                        Debug.WriteLine($"Hello, {id}!");
+                        var upass = reader.GetString(1);
+                        Debug.WriteLine($"Hello, {upass}!");
+                        var jobtitle = reader.GetString(2);
+                        Debug.WriteLine($"Hello, {jobtitle}!");
+
+                        if (txtUName.Text == id && txtUPass.Password.ToString() == upass)
+                        {
+                            //Login success
+                            if (jobtitle == "cashier")
+                            {
+                                //MainWinStaff w = new MainWinStaff();
+                                //w.Show();
+                            }
+                            else if (jobtitle == "manager")
+                            {
+                                //MainWinStaff w = new MainWinStaff();
+                                //w.Show();
+                            }
+
+                        }
+                        else
+                        {
+                            //Login failed
+                            Debug.WriteLine("wwww");
+                        }
+                    }
+                }
+            }
+
         }
     }
 }
